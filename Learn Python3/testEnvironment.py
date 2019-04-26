7c35d9fac70fcf8c35c0402f7f68e562041aba2a
Xyxeii <anthonymartiniano@gmail.com>
2019-04-26 13:19:07 -0400

    setup .gitignore

--- Learn Python3/testEnvironment.py
+++
@@ -1,8 +0,0 @@
-names = {"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}
-def count_first_letter(names):
-	new_dict = { k[0]:0 for k,v in names.items() }
-	for k,v in names.items():
-		new_dict[k[0]] += len(v)
-	return new_dict
-
-print(count_first_letter(names))
